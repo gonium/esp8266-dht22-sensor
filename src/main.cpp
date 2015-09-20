33fc3e4a89a2d21e51de1e9d0d5075a0a27e8d20
Mathias Dalheimer <md@gonium.net>
2015-09-20 17:22:27 +0200

    fully working DHT22 to JSON interface

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,55 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <DHT.h>
 #include "../config.h"
+#define DHTTYPE DHT22
+#define DHTPIN  2
 
 MDNSResponder mdns;
 ESP8266WebServer server(80);
 
-const int led = 2;
+// Initialize DHT sensor
+// NOTE: For working with a faster than ATmega328p 16 MHz Arduino chip, like an ESP8266,
+// you need to increase the threshold for cycle counts considered a 1 or 0.
+// You can do this by passing a 3rd parameter for this threshold.  It's a bit
+// of fiddling to find the right value, but in general the faster the CPU the
+// higher the value.  The default for a 16mhz AVR is a value of 6.  For an
+// Arduino Due that runs at 84mhz a value of 30 works.
+// This is for the ESP8266 processor on ESP-01
+DHT dht(DHTPIN, DHTTYPE, 11); // 11 works fine for ESP8266
 
-void handleRoot() {
-  digitalWrite(led, 1);
-  server.send(200, "text/plain", "hello from esp8266!");
-  digitalWrite(led, 0);
+float humidity, temp;  // Values read from sensor
+String webString="";     // String to display
+// Generally, you should use "unsigned long" for variables that hold time
+unsigned long previousMillis = 0;        // will store last temp was read
+const long interval = 2000;              // interval at which to read sensor
+
+static void gettemperature() {
+  // Wait at least 2 seconds seconds between measurements.
+  // if the difference between the current time and last time you read
+  // the sensor is bigger than the interval you set, read the sensor
+  // Works better than delay for things happening elsewhere also
+  unsigned long currentMillis = millis();
+ 
+  if(currentMillis - previousMillis >= interval) {
+    // save the last time you read the sensor 
+    previousMillis = currentMillis;
+ 
+    // Reading temperature for humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+    humidity = dht.readHumidity();          // Read humidity (percent)
+    temp = dht.readTemperature(false);     // Read temperature as Celsius
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(humidity) || isnan(temp)) {
+      Serial.println("Failed to read from DHT sensor!");
+      return;
+    }
+  }
 }
 
-void handleNotFound(){
-  digitalWrite(led, 1);
+
+static void handleNotFound(){
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
@@ -29,12 +63,9 @@ void handleNotFound(){
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
 }
  
 void setup(void){
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
   Serial.begin(9600);
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -44,8 +75,6 @@ void setup(void){
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-		// toggle LED
-		digitalWrite(led, !digitalRead(led));
   }
 
   Serial.println("");
@@ -55,25 +84,33 @@ void setup(void){
   Serial.println(WiFi.localIP());
 
   if (mdns.begin(hostname, WiFi.localIP())) {
-    Serial.print("MDNS responder started, using hostname ");
-		Serial.println(*hostname);
+    Serial.println("MDNS responder started");
   }
 
-  server.on("/", handleRoot);
-  server.on("/off", [](){
-		digitalWrite(led, 1);
-    server.send(200, "text/plain", "LED off");
+	server.on("/", [](){  // if you add this subdirectory to your webserver call, you get text below :)
+    gettemperature();       // read sensor
+		webString = "Sensor " + String(hostname) + " reports:\n";
+    webString+="Temperature: "+String(temp)+" degree Celsius\n";
+    webString+="Humidity: "+String(humidity)+" Percent";
+    server.send(200, "text/plain", webString);            // send to someones browser when asked
   });
-  server.on("/on", [](){
-		digitalWrite(led, 0);
-    server.send(200, "text/plain", "LED on");
+	server.on("/temperature", [](){  // if you add this subdirectory to your webserver call, you get text below :)
+    gettemperature();       // read sensor
+    webString="{\"temperature\": "+String(temp)+",\"unit\": \"Celsius\"}";
+    server.send(200, "text/plain", webString);            // send to someones browser when asked
   });
-  server.onNotFound(handleNotFound);
+ 
+  server.on("/humidity", [](){  // if you add this subdirectory to your webserver call, you get text below :)
+    gettemperature();           // read sensor
+    webString="{\"humidity\": "+String(humidity)+",\"unit\": \"Percent\"}";
+    server.send(200, "text/plain", webString);               // send to someones browser when asked
+  });
+	server.onNotFound(handleNotFound);
 
   server.begin();
-  Serial.println("HTTP server started");
 }
 
 void loop(void){
   server.handleClient();
 }
+
