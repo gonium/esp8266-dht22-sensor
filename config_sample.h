a12b85f6d965fc1b4d18a03fb65297a6071a1fe3
Mathias Dalheimer <md@gonium.net>
2015-09-20 13:30:18 +0200

    created config.h setup

---
+++ config_sample.h
@@ -0,0 +1,7 @@
+#ifndef CONFIG_H
+#define CONFIG_H 1
+
+const char* ssid = "******";
+const char* password = "******";
+
+#endif /* CONFIG_H */
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,8 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include "../config.h"
 
-const char* ssid = "******";
-const char* password = "******";
 MDNSResponder mdns;
 
 ESP8266WebServer server(80);
