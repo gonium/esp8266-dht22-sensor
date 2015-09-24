f8bad0e1cd515d845c94ce3ddb1bc1a3e6c67b23
Mathias Dalheimer <md@gonium.net>
2015-09-24 17:17:33 +0200

    added defines for different sensor types

--- config_sample.h
+++ config_sample.h
@@ -1,7 +1,11 @@
 #ifndef CONFIG_H
 #define CONFIG_H 1
 
-const char* ssid = "******";
-const char* password = "******";
+//#define SENSOR_DHT22 1
+#define SENSOR_DS18S20 1
+
+const char* ssid = "nibbler";
+const char* password = "0assvpanpv6ei";
+const char* hostname = "roomsensor";
 
 #endif /* CONFIG_H */
