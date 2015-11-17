f9e3c89a5d46bd4adeb5bcb621e3cb196de51d97
Mathias Dalheimer <md@gonium.net>
2015-11-17 22:08:14 +0100

    didn't copy change of hostname variable over into sample configuration

--- config_sample.h
+++ config_sample.h
@@ -3,16 +3,18 @@
 
 // This is the sensor definition field. Please comment *in* the 
 // Sensor that you have installed.
+// How many sensors are attached? Only 1 for the DHT22/DHT11 possible,
+// but many more for the DS18S20.
+#define MAX_NUM_SENSORS 1
 // Option 1: you have a DHT22/DHT11 temp/hum sensor.
 #define SENSOR_DHT22 1
 //#define DHTTYPE DHT22
 #define DHTTYPE DHT11
 // Option 2: you have an DS18S20 temperature sensor
 //#define SENSOR_DS18S20 1
-#define MAX_NUM_SENSORS 4
 
 const char* ssid = "****";
 const char* password = "****";
-const char* hostname = "roomsensor";
+const char* mdnsname = "roomsensor";
 
 #endif /* CONFIG_H */
